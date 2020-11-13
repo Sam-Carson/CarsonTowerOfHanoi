ee1daef1f5bd631fba80cec928cd680d68594be7
Sam Carson <49166726+Sam-Carson@users.noreply.github.com>
2020-11-12 18:51:23 -0800

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,52 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Towers_Should
+    {
+        [TestMethod]
+        public void ConstructTower_WhenNumberOfDisksIsDeclared()
+        {
+
+        }
+
+        [TestMethod]
+        public void ConstructTowerWithAllDisksOnLeftPole_WhenNumberOfDisksIsDeclared()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class Move_Should
+    {
+        [TestMethod]
+        public void MoveSmallestDiskToTopOfLargerDisk_WhenUserMovesDiskFromPole()
+        {
+
+        }
+
+        [TestMethod]
+        public void CatchExecption_WhenUserMovesDiskFromEmptyPole()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class ToArray_Should
+    {
+        [TestMethod]
+        public void ReturnJaggedArray_WhenCalled()
+        {
+
+        }
+
+        [TestMethod]
+        public void CreateJaggedArrayWithDimensionTwoBeingInts_WhenCalled()
+        {
+
+        }
+    }
+}
